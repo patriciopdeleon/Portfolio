cf27abdd7fe78e1e8d48bb97684e50ec2a44f517
Patricio Perez De Leon <patriciopdeleon@gmail.com>
2023-11-04 17:03:08 -0400

    portfolio v2

--- themeToggle.js
+++ themeToggle.js
@@ -1,29 +1,22 @@
-// Listen for a click event on the element with the id of "theme-toggle"
-/*document.getElementById('theme-toggle').addEventListener('click', function() {
-    // Toggle the "dark-mode" class on the body element
-    document.body.classList.toggle('dark-mode');
-});
-*/
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('theme-toggle');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-    // Function to toggle the theme
-    function toggleTheme() {
-        document.body.classList.toggle('dark-mode');
-        // Save theme preference in localStorage
-        if (document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('theme', 'dark-mode');
-        } else {
-            localStorage.removeItem('theme');
-        }
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
     }
+}
 
-    // Event listener for the button
-    toggleButton.addEventListener('click', toggleTheme);
-
-    // Check localStorage for theme preference and apply it
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        document.body.classList.add(savedTheme);
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
-});
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
